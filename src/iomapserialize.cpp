110bf6907f36471a8daa82386641e9f41c565e4f
Max Farr <maxandersfarr@gmail.com>
2024-04-23 16:48:27 -0700

    applying fix for fmt enum issue

--- src/iomapserialize.cpp
+++ src/iomapserialize.cpp
@@ -312,7 +312,7 @@ bool IOMapSerialize::saveHouseInfo()
 
 		std::string listText;
 		if (house->getAccessList(GUEST_LIST, listText) && !listText.empty()) {
-			if (!stmt.addRow(fmt::format("{:d}, {}, {:s}", house->getId(), GUEST_LIST, db.escapeString(listText)))) {
+			if (!stmt.addRow(fmt::format("{:d}, {:d}, {:s}", house->getId(), static_cast<int>(GUEST_LIST), db.escapeString(listText)))) {
 				return false;
 			}
 
@@ -320,7 +320,7 @@ bool IOMapSerialize::saveHouseInfo()
 		}
 
 		if (house->getAccessList(SUBOWNER_LIST, listText) && !listText.empty()) {
-			if (!stmt.addRow(fmt::format("{:d}, {}, {:s}", house->getId(), SUBOWNER_LIST, db.escapeString(listText)))) {
+			if (!stmt.addRow(fmt::format("{:d}, {:d}, {:s}", house->getId(), static_cast<int>(SUBOWNER_LIST), db.escapeString(listText)))) {
 				return false;
 			}
 
